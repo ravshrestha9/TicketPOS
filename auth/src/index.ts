eb2db3f8908b4a9e2bb6eac940837b17e452c4df
ravshrestha9 <shrestharavee1@gmail.com>
2020-09-13 12:27:41 -0500

    Add Mongoose and its respective deployment and service to create pods

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,6 +1,8 @@
 import express from "express";
 import 'express-async-errors';
 import { json } from "body-parser";
+import mongoose from 'mongoose';
+
 import {currentUserRouter} from './routes/current-user';
 import {signinRouter} from './routes/signin';
 import {signupRouter} from './routes/signup';
@@ -22,6 +24,23 @@ app.all('*', async(req, res)=>{
 
 app.use(errorHandler);
 
+const start = async() => {
+  try{
+    await mongoose.connect('mongodb://auth-mongo-srv:27017/auth', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true
+  });
+  console.log('Connected to MongoDB...')
+}
+catch(err){
+  console.error(err);
+}
+
 app.listen(3000, () => {
   console.log("Listening in port 3000!!");
 });
+
+};
+
+start();
