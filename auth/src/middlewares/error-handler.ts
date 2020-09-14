32841fbf0c43444546e7725e6286d6995d7e092d
ravshrestha9 <shrestharavee1@gmail.com>
2020-09-13 20:18:34 -0500

    Adding bad request handling, password hashing, User Model and schema for user

--- auth/src/middlewares/error-handler.ts
+++ auth/src/middlewares/error-handler.ts
@@ -11,9 +11,9 @@ export const errorHandler = (
    if(err instanceof CustomError){
     return res.status(err.statusCode).send({errors: err.serializeErrors()});
    }
-    
+   
    res.status(400).send({
     errors: [{message: 'Something went wrong'}]
    });
 
-}; 
+};
--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,7 +1,8 @@
 import express, {Response, Request} from 'express';
 import {body, validationResult} from 'express-validator';
+import {User} from '../models/user';
 import {RequestValidationError} from '../errors/request-validation-error';
-import {DatabaseConnectionError} from '../errors/database-connection-error';
+import {BadRequestError} from '../errors/bad-request-error';
 
 const router = express.Router();
 
@@ -21,13 +22,18 @@ router.post('/api/users/signup',[
   throw new RequestValidationError(errors.array());
  }
 
- console.log('Creating a user...');
+ const { email, password } = req.body;
 
- throw new DatabaseConnectionError();
+    const existingUser = await User.findOne({ email });
 
- res.send({});
+    if (existingUser) {
+      throw new BadRequestError('Email in use');
+    }
 
+    const user = User.build({ email, password });
+    await user.save();
 
+    res.status(201).send(user);
 });
 
 export {router as signupRouter};
