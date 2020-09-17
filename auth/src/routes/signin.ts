211259835e836259fa2d2c833fcffce746c2c2cc
ravshrestha9 <shrestharavee1@gmail.com>
2020-09-16 22:21:23 -0500

    Refactor and add email validation to sigin

--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -1,9 +1,29 @@
-import express from 'express';
+import express, {Request, Response} from 'express';
+import {body} from 'express-validator';
 
+import {User} from '../models/user';
+import { validateRequest } from '../middlewares/validate-request';
+import { BadRequestError } from '../errors/bad-request-error';
+import { Password } from '../services/password';
 const router = express.Router();
 
-router.post('/api/users/signin', (req, res)=>{
- res.send('Hi There!');
+router.post('/api/users/signin', [
+ body('email')
+  .isEmail()
+  .withMessage('Email must be valid'),
+  body('password')
+   .trim()
+   .notEmpty()
+   .withMessage('You must supply a message.')],validateRequest,
+async (req: Request, res: Response)=>{
+ const {email, password} = req.body;
+
+ const existingUser = await User.findOne({email});
+ if (!existingUser){
+   throw new BadRequestError('Invalid credentials!!!');
+ }
+
+ const passwordsMatch = await Password.compare(existingUser.password, password); 
 });
 
 export {router as signinRouter};
--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,7 +1,8 @@
 import express, {Response, Request} from 'express';
-import {body, validationResult} from 'express-validator';
+import {body} from 'express-validator';
 import {User} from '../models/user';
-import {RequestValidationError} from '../errors/request-validation-error';
+
+import {validateRequest} from '../middlewares/validate-request';
 import {BadRequestError} from '../errors/bad-request-error';
 import jwt from 'jsonwebtoken';
 
@@ -15,17 +16,10 @@ router.post('/api/users/signup',[
   .trim()
   .isLength({min:4, max:20})
   .withMessage('Password must be between 4 and 20 characters')
-],
+],validateRequest,
  async(req: Request, res: Response)=>{
- const errors = validationResult(req);
-
- if (!errors.isEmpty()){
-  throw new RequestValidationError(errors.array());
- }
-
  const { email, password } = req.body;
-
-    const existingUser = await User.findOne({ email });
+ const existingUser = await User.findOne({ email });
 
     if (existingUser) {
       throw new BadRequestError('Email in use');
