31fac8d9f64c3e8480acd1bb6c899d383223b8f9
ravshrestha9 <shrestharavee1@gmail.com>
2020-09-22 21:29:00 -0500

    Add unit tests to the auth routes

--- auth/src/routes/__test__/signup.test.ts
+++ auth/src/routes/__test__/signup.test.ts
@@ -11,8 +11,62 @@ it('returns a 201 on successful signup', async()=>{
  .expect(201);
 });
 
-// describe('Sample Test', () => {
-//  it('should test that true === true', () => {
-//    expect(true).toBe(true)
-//  })
-// })
+it('returns a 400 with an invalid email', async()=>{
+  return request(app)
+  .post('/api/users/signup')
+  .send({
+   email: 'testtest.com',
+   password: 'password'
+  })
+  .expect(400);
+ })
+
+
+ it('returns a 400 with an invalid password', async()=>{
+  return request(app)
+  .post('/api/users/signup')
+  .send({
+   email: 'test@test.com',
+   password: 'p'
+  })
+  .expect(400);
+ })
+
+ it('returns a 400 with missing email and password', async()=>{
+  return request(app)
+  .post('/api/users/signup')
+  .send({  
+  })
+  .expect(400);
+ })
+
+ it('disallows duplicate emails', async() =>{
+  await request(app)
+  .post('/api/users/signup')
+  .send({
+   email: 'test@test.com',
+   password: 'password'
+  })
+  .expect(201);
+
+  await request(app)
+  .post('/api/users/signup')
+  .send({
+   email: 'test@test.com',
+   password: 'password'
+  })
+  .expect(400);
+ })
+
+ it('sets a cookie after a successful signup', async()=>{
+ const response = await request(app)
+  .post('/api/users/signup')
+  .send({
+   email: 'test@test.com',
+   password: 'password'
+  })
+  .expect(201);
+
+  expect(response.get('Set-Cookie')).toBeDefined();
+ });
+ 
--- auth/src/test/setup.ts
+++ auth/src/test/setup.ts
@@ -1,10 +1,17 @@
 import { MongoMemoryServer } from "mongodb-memory-server";
 import mongoose from 'mongoose';
 import { app } from '../app';
+import request from 'supertest';;
 
+
+declare global {
+  namespace NodeJS{
+    interface Global{
+      signin(): Promise<string[]>
+    }
+  }
+}
 let mongo: any;
-
-
 
 beforeAll(async () => {
   process.env.JWT_KEY = 'asdf';
@@ -30,3 +37,18 @@ afterAll(async () => {
   await mongoose.connection.close();
 });
 
+global.signin = async()=>{
+  const email = 'test@test.com';
+  const password = 'password';
+
+  const response = await request(app)
+    .post('/api/users/signup')
+    .send({
+      email,password
+    })
+    .expect(201);
+
+  const cookie = response.get('Set-Cookie');
+
+  return cookie;
+};
