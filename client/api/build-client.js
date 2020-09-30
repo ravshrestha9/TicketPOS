35384282eaa8c0ce3811b46e55b268b8cafbb0b6
ravshrestha9 <shrestharavee1@gmail.com>
2020-09-29 22:06:39 -0500

    revert back

--- client/api/build-client.js
+++ client/api/build-client.js
@@ -4,7 +4,7 @@ export default ({req}) =>{
  if(typeof window === 'undefined'){
   //We are on the server
   //If in server request needs to be directed to
- // http://service.namespace.svc.cluster.local z
+ // http://service.namespace.svc.cluster.local
   return axios.create({
    baseURL: 'http://ingress-nginx.ingress-nginx.svc.cluster.local',
    headers: req.headers
