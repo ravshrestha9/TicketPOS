5082455e65fbd3867805ebf79b6ce962a8946891
ravshrestha9 <shrestharavee1@gmail.com>
2020-09-30 22:08:53 -0500

    Debugging in local kubernetes cluster. Issue still persists

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,16 +1,30 @@
-import buildClient from '../api/build-client'
+//import buildClient from '../api/build-client'
+
+import axios from "axios";
 
 const LandingPage = ({currentUser}) => {
  console.log(currentUser);
- //axios.get('/api/users/currentuser');
+ //Axios.get('/api/users/currentuser');
  return <h1>Landing Page</h1>;
 };
 
-LandingPage.getInitialProps = async (context) => {
- const client = buildClient(context);
- const {data} = await client.get('/api/users/currentuser');
+//async (context) 
+LandingPage.getInitialProps = async() => {
+//  // const client = buildClient(context);
+//  // const {data} = await client.get('/api/users/currentuser');
 
+//  // return data;
+
+//  const response = await Axios.get('/api/users/currentuser');
+
+if(typeof window === 'undefined'){
+ const {data} = await axios.get('http://ingress-nginx.ingress-nginx.svc.cluster.local/api/users/currentuser');
  return data;
+}else{
+ const response = await axios.get('/api/users/currentuser');
+ return response.data;
+}
+ return {};
 };
 
 export default LandingPage;
