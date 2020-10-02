d54a4103336c8097e8405ef6a4cbe140e798ee0a
ravshrestha9 <shrestharavee1@gmail.com>
2020-10-01 22:04:01 -0500

    Debugging ingress

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,30 +1,16 @@
-//import buildClient from '../api/build-client'
-
-import axios from "axios";
+import buildClient from '../api/build-client'
 
 const LandingPage = ({currentUser}) => {
  console.log(currentUser);
- //Axios.get('/api/users/currentuser');
+ //axios.get('/api/users/currentuser');
  return <h1>Landing Page</h1>;
 };
 
-//async (context) 
-LandingPage.getInitialProps = async() => {
-//  // const client = buildClient(context);
-//  // const {data} = await client.get('/api/users/currentuser');
+LandingPage.getInitialProps = async (context) => {
+ const client = buildClient(context);
+ const {data} = await client.get('/api/users/currentuser');
 
-//  // return data;
-
-//  const response = await Axios.get('/api/users/currentuser');
-
-if(typeof window === 'undefined'){
- const {data} = await axios.get('http://ingress-nginx.ingress-nginx.svc.cluster.local/api/users/currentuser');
  return data;
-}else{
- const response = await axios.get('/api/users/currentuser');
- return response.data;
-}
- return {};
 };
 
 export default LandingPage;
