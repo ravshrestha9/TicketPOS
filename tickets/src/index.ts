b90e4b8cffdd95149f1a1ec2d3f4f81c8bb1f3e9
ravshrestha9 <shrestharavee1@gmail.com>
2020-10-06 22:03:04 -0500

    Add tests and routes for tickets service

--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -6,8 +6,12 @@ const start = async() => {
     throw new Error('JWT Key needs to be defined...')
   }
 
+  if(!process.env.MONGO_URI){
+    throw new Error('Mongo Uri needs to be defined');
+  }
+
   try{
-    await mongoose.connect('mongodb://tickets-mongo-srv:27017/auth', {
+    await mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true
