6a7adcf7383bff828811b20aa70224ea16a9946b
ravshrestha9 <shrestharavee1@gmail.com>
2020-10-09 21:52:40 -0500

    Add get and update ticket routes

--- tickets/src/routes/new.ts
+++ tickets/src/routes/new.ts
@@ -1,6 +1,6 @@
 import express, { Request, Response } from 'express';
 import { body } from 'express-validator';
-
+import { Ticket } from '../models/ticket';
 import { requireAuth,validateRequest  } from '@shrestr3/common';
 
 const router = express.Router();
@@ -12,8 +12,18 @@ router.post('/api/tickets', requireAuth,  [
    .withMessage('Price must be greater than 0'),
 ],
 validateRequest,
-(req: Request, res: Response) => {
-  res.sendStatus(200);
+async(req: Request, res: Response) => {
+ const { title, price } = req.body;
+
+ const ticket = Ticket.build({
+   title,
+   price,
+   userId: req.currentUser!.id,
+ });
+ await ticket.save();
+
+ res.status(201).send(ticket);
+
 });
 
 export { router as createTicketRouter };
