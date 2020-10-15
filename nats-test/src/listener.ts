c49f0f5eee7a62da88b5ac5d38e35d55d9bbf6b9
ravshrestha9 <shrestharavee1@gmail.com>
2020-10-14 21:30:24 -0500

    Add graceful client shutdown

--- nats-test/src/listener.ts
+++ nats-test/src/listener.ts
@@ -3,20 +3,35 @@ import { randomBytes } from 'crypto';
 
 console.clear();
 
-const stan = nats.connect('ticketing', '123', {
+const stan = nats.connect('ticketing', randomBytes(4).toString('hex'), {
   url: 'http://localhost:4222',
 });
 
 stan.on('connect', () => {
   console.log('Listener connected to NATS');
+  
+  stan.on('close', () => {
+    console.log('NATS connection closed!');
+    process.exit();
+  });
 
-  const subscription = stan.subscribe('ticket:created');
+  const options = stan.subscriptionOptions().setManualAckMode(true);
+  const subscription = stan.subscribe(
+    'ticket:created',
+    'orders-service-queue-group',
+    options
+  );
 
-  subscription.on('message', (msg) => {
-   const data = msg.getData();
+  subscription.on('message', (msg: Message) => {
+    const data = msg.getData();
 
-   if (typeof data === 'string') {
-     console.log(`Received event #${msg.getSequence()}, with data: ${data}`);
-   }
+    if (typeof data === 'string') {
+      console.log(`Received event #${msg.getSequence()}, with data: ${data}`);
+    }
+
+    msg.ack();
   });
 });
+
+process.on('SIGINT', () => stan.close());
+process.on('SIGTERM', () => stan.close());
